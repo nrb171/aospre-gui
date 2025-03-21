09df6069c9f9809307c1e25da38ad2a9903cc4e7
Nicholas Barron <nbarron@ucar.edu>
2025-03-21 16:20:02 -0400

    Improvements to earth/simulation-space communication and plotting

--- helpers/nuimagesc.m
+++ helpers/nuimagesc.m
@@ -3,30 +3,53 @@ function [imsc] = nuimagesc(ax,xlat, xlon, var)
 %   handles image plotting, but with non-orthogonal grids, such as often is
 %   found in numerical weather simulations. This is substantially faster
 %   than alternatives like PCOLOR(), which uses surfaces instead of images.
-
-    [py,px] = meshgrid(1:size(xlat,2), 1:size(xlat,1));
-    xRescaled = normalize(xlon(:), 1, 'range', [1, size(xlon,1)]);
-    xRescaled = reshape(xRescaled, size(xlon));
+    xlat2 = xlat;
+    xlon2 = xlon;
+    var2 = var;
+    indsi = round(linspace(1,size(xlon,1),31));
+    indsj = round(linspace(1,size(xlon,2),31));
+    hold(ax, 'on')
+    for ii = 1:numel(indsi)-1
+        for jj = 1:numel(indsj)-1
+            xlat = xlat2(...
+                max(indsi(ii)-2,1):min(indsi(ii+1)+2, size(xlat2,1)), ...
+                max(indsj(jj)-2,1):min(indsj(jj+1)+2, size(xlat2,2)));
+            xlon = xlon2(...
+                max(indsi(ii)-2,1):min(indsi(ii+1)+2, size(xlat2,1)), ...
+                max(indsj(jj)-2,1):min(indsj(jj+1)+2, size(xlat2,2)));
+            var = var2(...
+                max(indsi(ii)-2,1):min(indsi(ii+1)+2, size(xlat2,1)), ...
+                max(indsj(jj)-2,1):min(indsj(jj+1)+2, size(xlat2,2)));
     
+            [py,px] = meshgrid(linspace(0,1,size(xlat,2)), linspace(0,1,size(xlat,1)));
+            xRescaled = normalize(xlon(:), 1, 'range', [0,1]);
+            xRescaled = reshape(xRescaled, size(xlon));
+            
+            
+            yRescaled = normalize(xlat(:), 1, 'range', [0, 1]);
+            yRescaled = reshape(yRescaled, size(xlon));
+            
+            d = cat(3, (yRescaled-py)*size(xRescaled,2), (xRescaled-px)*size(xRescaled,1));
+             
+            im = imwarp(var,-d*1, 'cubic');
+            %im = im(sum(im,2) ~= 0, sum(im,1) ~= 0);
+            
+            alphaData = ones(size(im))*1;
+            alphaData(im == 0) = 0;
+            alphaData(isnan(im)) = 0;
+            
+            % close all
+        
+            imagesc(ax,...
+                [min(xlon(:)), max(xlon(:))], ...
+                [min(xlat(:)), max(xlat(:))], ...
+                im', ...
+                "AlphaData", alphaData' ...
+            );
+            
+        end
+    end
     
-    yRescaled = normalize(xlat(:), 1, 'range', [1, size(xlon,2)]);
-    yRescaled = reshape(yRescaled, size(xlon));
-    
-    d = cat(3, (yRescaled-py), (xRescaled-px));
-     
-    im = imwarp(var,-d);
-    
-    alphaData = ones(size(im))*0.8;
-    alphaData(im == 0) = 0;
-    alphaData(isnan(im)) = 0;
-    
-    % close all
-    imsc=imagesc(ax,...
-        [min(xlon(:)), max(xlon(:))], ...
-        [min(xlat(:)), max(xlat(:))], ...
-        im', ...
-        "AlphaData", alphaData' ...
-    );
     set(ax, 'YDir','normal') 
 end
 
