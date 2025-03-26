4b6cbe4466a28020adf85e89b9fa9adad126bb7e
Nicholas Barron <nbarron@ucar.edu>
2025-03-26 10:10:50 -0400

    helper script to abbreviate datasets

--- helpers/extractAbbreviatedVariables.m
+++ helpers/extractAbbreviatedVariables.m
@@ -13,25 +13,18 @@ if ~exist(abbreviatedFolder, 'dir')
     mkdir(abbreviatedFolder)
 end
 
+%% create variable slice at 2 km
 for iFile = 1:numel(app.FileList)
     nci = ncinfo([app.FileList(iFile).folder, '/', app.FileList(iFile).name]);
-    
-    
-
-
     allVarNames = string({nci.Variables.Name});
     for varName = [requiredVariables, variablesToKeep]
-
-        
-
         varData = ncread([app.FileList(iFile).folder, '/', app.FileList(iFile).name], varName);
-        
         varIndx = find(allVarNames == varName);
         varDimNames = string({nci.Variables(varIndx).Dimensions.Name});
         varDimLength = [nci.Variables(varIndx).Dimensions.Length];
         varDimCell = {};
         for iDim = 1:length(varDimLength)
-            if ~contains(varDimNames, 'bottom_top')
+            if ~contains(varDimNames(iDim), 'bottom_top')
                 varDimCell{end+1} = varDimNames(iDim);
                 varDimCell{end+1} = varDimLength(iDim);
             else
@@ -39,15 +32,11 @@ for iFile = 1:numel(app.FileList)
                 varDimCell{end+1} = 1;
             end
         end
-
         nccreate([abbreviatedFolder,app.FileList(iFile).name], varName, "Dimensions",varDimCell)
         if numel(varDimCell) == 8
-        ncwrite([abbreviatedFolder,app.FileList(iFile).name], varName, varData)
-
-        
-        
+            ncwrite([abbreviatedFolder,app.FileList(iFile).name], varName, varData(:,:,28,:))
+        else
+            ncwrite([abbreviatedFolder,app.FileList(iFile).name], varName, varData)
+        end 
     end
-
-
-
 end
